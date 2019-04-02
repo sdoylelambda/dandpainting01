0bd94a7f155f068ffab14c7f322d52fa5ffc3de2
Sean Doyle <sdoylelambda@gmail.com>
2019-04-02 09:14:54 -0400

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,52 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+// code for accordian
+class Panel{
+	constructor(panel) {
+	  // get panel
+	  this.panel = panel;
+	  // get all different button options needed
+	  this.panelButtons = this.panel.querySelector('.panel-buttons');
+	  this.panelBtnOpen = this.panel.querySelector('.panel-btn-open');
+	  this.panelBtnClosed = this.panel.querySelector('.panel-btn-close');
+	  this.panelContent = this.panel.querySelector('.panel-content');
+	  
+	  // use button options 2x
+	 // this.panelButtons.addEventListener('click', this.togglePanel.bind(this));
+	  this.panelButtons.addEventListener('click', () => this.togglePanel());
+	}
+	// Methods
+	togglePanel() {
+	  this.panelBtnOpen.classList.toggle('hide-btn');
+	  this.panelBtnClosed.classList.toggle('hide-btn');
+	  this.panelContent.classList.toggle('toggle-on');
+	}
+  }
+  
+  // Step 1 : get orginal DOM elements
+  const panels = document.querySelectorAll('.panel');
+  
+  // Step 2 : return a newly constructed DOM element
+  panels.forEach( function(panel){
+	return new Panel(panel);
+  });
+  
+
+// end of my code
+
+
+
+
+
+
+
+
+
+
+
+
+
 (function($) {
 
 	var $window = $(window),
